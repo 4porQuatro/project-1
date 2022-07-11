8c9a92632b65ab7dce4d324008170b634037b6e5
Vilels <pedrofilipevilela96@gmail.com>
2022-07-11 15:52:14 +0100

    Update section_with_two_images_on_left.blade.php

--- resources/views/front/components/features/section_with_two_images_on_left.blade.php
+++ resources/views/front/components/features/section_with_two_images_on_left.blade.php
@@ -26,22 +26,10 @@
                     </div>
                     <div class="px-lg-5">
                         <p class="h5 mb-5">
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                            diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                            aliquyam.
+                            {{$data->subtitle->default}}
                         </p>
                         <p class="h6">
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                            diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                            aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                            justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-                            sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-                            ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-                            nonumy eirmod tempor invidunt ut labore et dolore magna
-                            aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                            justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-                            sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem
-                            ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.
+                            {!! $data->body->default !!}
                         </p>
                     </div>
                 </div>
